c77acc6458d9a9ec01c2412e950300dbaf5d9dac
yuchi <yuchi8943@gmail.com>
2023-03-07 16:52:34 +0800

    顯示尚未完成數量

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -113,6 +113,20 @@ export class TodoListComponent implements OnInit {
     todo.editable = false ;
   }
 
+
+  /**
+ * 取得未完成的待辦事項清單
+ *
+ * @returns {Todo[]}
+ * @memberof TodoListComponent
+ */
+  getRemainingList(): Todo[] {
+    return this.todoListService.getWithCompleted(false);
+  }
+
+ 
+
+
 }
 
 
